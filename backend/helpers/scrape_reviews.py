5e036187345799a363d5e87dd56d930b626968c2
dvchavan <dvchavan@iu.edu>
2025-11-26 21:50:02 -0500

    no longer scraping description, only name, date, user review, and user rating

--- backend/helpers/scrape_reviews.py
+++ backend/helpers/scrape_reviews.py
@@ -1,47 +1,49 @@
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 import sys
 import pandas as pd
 import os
-from .utils import create_path
-import requests
-from bs4 import BeautifulSoup
+from itertools import count
 
-def click_element(driver, element):
+def click_element(driver, element) -> None:
     driver.execute_script('arguments[0].click();', element)
 
-def get_url(username: str, page_number: int):
+def get_url(username: str, page_number: int) -> str:
     return f'https://letterboxd.com/{username}/reviews/films/page/{page_number}/'
 
+def reveal_hidden_content(driver: webdriver.Chrome, review: WebElement) -> None:
+    try:
+        reveal_spoiler_button = review.find_element(By.CSS_SELECTOR, '[data-js-trigger="spoiler.reveal"]')
+        click_element(driver, reveal_spoiler_button)
+    except:
+        pass
+    try:
+        show_more_button = review.find_element(By.CLASS_NAME, 'reveal')
+        click_element(driver, show_more_button)
+    except:
+        pass
+
 def scrape_reviews(username: str, print_status: bool = False) -> pd.DataFrame:
-    if print_status:
-        print('starting scraping')
+    if print_status: print('starting scraping')
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options)
-    columns = ['name', 'user rating', 'user review', 'date', 'description']
-    data = {}
-    for column in columns:
-        data[column] = []
-    page_number = 1
-    while True:
-        if print_status:
-            print(f'scraping page {page_number}...')
+    columns = ['name', 'date', 'user rating', 'user review']
+    data = { col:[] for col in columns }
+    for page_number in count(start=1, step=1):
+        if print_status: print(f'scraping page {page_number}...')
         driver.get(get_url(username, page_number))
         reviews = driver.find_elements(By.CSS_SELECTOR, '[data-object-name="review"]')
         if len(reviews) == 0:
             break
         for review in reviews:
-            try:
-                reveal_spoiler_button = review.find_element(By.CSS_SELECTOR, '[data-js-trigger="spoiler.reveal"]')
-                click_element(driver, reveal_spoiler_button)
-            except:
-                pass
-            try:
-                show_more_button = review.find_element(By.CLASS_NAME, 'reveal')
-                click_element(driver, show_more_button)
-            except:
-                pass
+            reveal_hidden_content(driver, review)
+            name_parent_element = review.find_element(By.CLASS_NAME, 'name')
+            name = name_parent_element.find_element(By.TAG_NAME, 'a').text
+            data['name'].append(name)
+            release_date = review.find_element(By.CLASS_NAME, 'releasedate').text
+            data['date'].append(release_date)
             try:
                 user_rating_symbols = review.find_element(By.CLASS_NAME, 'rating').text.strip()
                 user_rating = user_rating_symbols.count('★') + \
@@ -50,31 +52,17 @@ def scrape_reviews(username: str, print_status: bool = False) -> pd.DataFrame:
                 # default to 0 which cannot be used as a rating in letterboxd
                 user_rating = 0
             data['user rating'].append(user_rating)
-            poster = review.find_element(By.CLASS_NAME, 'film-poster')
-            movie_link = poster.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            description = None
-            if movie_link:
-                response = requests.get(movie_link)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                description_element = soup.find(attrs={'class':'truncate'})
-                if description_element:
-                    description = description_element.text.strip()
-            data['description'].append(description)
-            name_parent_element = review.find_element(By.CLASS_NAME, 'name')
-            name = name_parent_element.find_element(By.TAG_NAME, 'a').text
-            data['name'].append(name)
             user_review_element = review.find_element(By.CLASS_NAME, 'js-review-body')
             user_review = user_review_element.text
             data['user review'].append(user_review)
-            release_date = review.find_element(By.CLASS_NAME, 'releasedate').text
-            data['date'].append(release_date)
-        page_number += 1
-    if print_status:
-        print('finished scraping')
+    if print_status: print('finished scraping')
     driver.quit()
     return pd.DataFrame.from_dict(data)
 
 if __name__ == "__main__":
+    from utils import create_path
     username = sys.argv[1]
     data = scrape_reviews(username, print_status=True)
     data.to_csv(create_path([os.getcwd(), '..', 'data', 'reviews', f'{username}.csv']), index=False)
+else:
+    from .utils import create_path
