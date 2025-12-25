2682e23c0141ebfa120930c2e44ed6ad5221ddcc
MysticalRobot <dhruv21420@gmail.com>
2025-12-24 20:26:40 -0500

    fixed bug caused by changed scraped review data feature names

--- backend/helpers/movieswreviews.py
+++ backend/helpers/movieswreviews.py
@@ -7,14 +7,14 @@ import pandas as pd
 def merge_sentiment_reviews_dataset(movies: pd.DataFrame, reviews: pd.DataFrame) -> pd.DataFrame:
     """Merges the features of `movies` with `reviews`: the result of sentiment analysis"""
     clean_title = lambda s: "" if pd.isna(s) else " ".join(re.sub(r"[^a-z0-9 ]", " ", s.lower()).split())
-    information = reviews[["year", "compound", "user_rating"]].itertuples()
-    clean_titles = reviews["name"].apply(clean_title).to_list()
+    information = reviews[["release_year", "compound", "user_rating"]].itertuples()
+    clean_titles = reviews["original_title"].apply(clean_title).to_list()
     title2info = {title:info for title, info in zip(clean_titles, information)}
     rows = []
     for movie in movies.itertuples():
         clean_title = movie.clean_title
         release_year = movie.release_year
-        if clean_title in title2info and float(title2info[clean_title].year) == release_year:
+        if clean_title in title2info and float(title2info[clean_title].release_year) == release_year:
             rows.append([
                     movie.imdb_id,
                     title2info[clean_title].compound,
